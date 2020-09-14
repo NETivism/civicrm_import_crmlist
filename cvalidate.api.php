35411b704766f40b1477025268d5e01f12af870d
hoshi <hoshi@netivism.com.tw>
2020-09-14 17:39:07 +0800

    refs #7059, modify birthday format change and add description

--- cvalidate.api.php
+++ cvalidate.api.php
@@ -95,6 +95,9 @@ function cvalidate_birthday($str) {
   if (!strtotime($str)) {
     return FALSE;
   }
+  if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$str)){
+    return FALSE;
+  }
   $bir = date("Y-m-d", strtotime($str));
   return $bir;
 }
