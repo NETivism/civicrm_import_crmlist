827ff8c9be5aec75ca3cb62bc1b92abdc3aa7bc8
Jimmy Huang <jimmy@netivism.com.tw>
2021-03-30 11:52:04 +0800

    refs #30259, fixes date formatting, repect original input on excel date format

--- cvalidate.api.php
+++ cvalidate.api.php
@@ -95,7 +95,7 @@ function cvalidate_birthday($str) {
   if (!strtotime($str)) {
     return FALSE;
   }
-  if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$str)){
+  if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s*(\d+:\d+:\d+)?$/",$str)){
     return FALSE;
   }
   $bir = date("Y-m-d", strtotime($str));
